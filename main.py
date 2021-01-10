3d8501ffc1f9679754adb6bafe292d402eab1668
Sharon Yang <yangshuai0211@gmail.com>
2021-01-09 23:56:52 -0500

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,29 @@
-from kivy.app import App #main object
-from kivy.lang import Builder # connecting python with kivy (importing kv to py)
+from kivy.app import App 
+from kivy.lang import Builder
 from kivy.uix.screenmanager import ScreenManager, Screen
-#from kivy.uix.gridlayout import GridLayout (importing kv to py)
 import json, glob, random
 from datetime import datetime
 from pathlib import Path
 from hoverable import HoverBehavior
+from kivy.uix.image import Image
+from kivy.uix.behaviors import ButtonBehavior
+from kivy.animation import Animation
 
 Builder.load_file("design.kv")
 
 class LoginScreen(Screen):
     def sign_up(self):
-        self.manager.current = "sign_up_screen"  #self means LoginScreen object(instance) in this case
-        #manager is method of Screen; LoginScreen is parent class, Screen is child calss
+        self.manager.current = "sign_up_screen" 
 
     def login(self, uname, pword):
          with open("users.json") as file:
              users = json.load(file)
-         if uname in users and users[uname]['password'] == pword:  #print(users.keys())
+         if uname in users and users[uname]['password'] == pword:  
              self.manager.current = 'login_screen_success'
          else:
-             self.ids.login_wrong.text="Wrong username or password!" #text object
+             anim = Animation(color = (0.6, 0.7, 0.1, 1))
+             anim.start(self.ids.login_wrong)
+             self.ids.login_wrong.text="Wrong username or password!" 
 
 class RootWidget(ScreenManager):
     pass
@@ -29,14 +32,11 @@ class SignUpScreen(Screen):
     def add_user(self,uname, pword):
         with open("users.json") as file:
             users = json.load(file)
-        #print(users) #{'user1':{}, 'user2':{}}
 
         users[uname]={'username':uname,'password':pword,
             'created': datetime.now().strftime("%Y-%m-%d %H-%M-%S") }
-        #print(users)
 
         with open("users.json", 'w') as file:#overwrite the users 
-            # after line 28, a completely new empty file is going to be created
             json.dump(users, file)
         
         self.manager.current = "sign_up_screen_success" 
@@ -52,42 +52,23 @@ class LoginScreenSuccess(Screen):
         self.manager.current = "login_screen" 
 
     def get_quote(self, feel):
-        #print(feel)
         feel = feel.lower()
         available_feelings = glob.glob("quotes/*txt") 
-        #print(available_feelings)#['quotes/happy.txt','quotes/sad.txt',..]
-        #['quotes\\happy.txt','quotes\\sad.txt',..]
         available_feelings=[Path(filename).stem for filename in 
                                  available_feelings]
-        print(available_feelings) #['happy','sad','unloved']
-        print(feel)
         if feel in available_feelings:
             with open("quotes/{feel}.txt") as file:
                 quotes = file.readlines()
-            print(quotes)  #["...", "....."]
             self.ids.quote.text = random.choice(quotes)
         else:
             self.ids.quote.text="Try anotehr feeling"
+            
+class ImageButton(ButtonBehavior, HoverBehavior, Image):
+    pass
 
 class MainApp(App):
     def build(self):
         return RootWidget()
 
-if __name__ == "__main__": # IMPORTANT???
+if __name__ == "__main__":
     MainApp().run()
-    
-# if __name__ == '__main__':
-#     MainApp().run()
-
-#$ pip uninstall numpy
-#$ pip install numpy==1.19.3
-# $ cd "/c/Program Files (x86)/Python37-32"
-# Sharon@Sharon_Yang MINGW64 /c/Program Files (x86)/Python37-32
-# $ source kivy_venv/Scripts/activate
-# (kivy_venv)
-# Sharon@Sharon_Yang MINGW64 /c/Program Files (x86)/Python37-32
-# $ python /e/Python/main.py
-# Sharon@Sharon_Yang MINGW64 /c/Program Files (x86)/Python37-32
-# $ python /e/Python/main.py
-
-# shift+Alt+F to format this file
